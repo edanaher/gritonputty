13e5894c9c32fb8e8db5011211df9bcaa7c9794e
Evan Danaher <edanaher@edanaher.net>
2016-01-28 14:54:14 -0500

    Replace data-state-path attribute with id.  Much cleaner.

--- state.js
+++ state.js
@@ -8,7 +8,7 @@ state = {
       console.log("Bad target for updateString: ", elem, "under", par);
       return;
     }
-    var path = par.attributes["data-state-path"].value;
+    var path = par.id;
     var letter = elem.attributes["data-state-char"].value;
     if(elem.classList.contains("active")) {
       elem.classList.remove("active");
@@ -24,7 +24,7 @@ state = {
     var strings = document.querySelectorAll("[data-state-type=string]");
     for(var i = 0; i < strings.length; i++) {
       var elem = strings[i];
-      var path = elem.attributes["data-state-path"].value;
+      var path = elem.id;
       for(var j = 0; j < elem.children.length; j++) {
         var child = elem.children[j];
         var c = child.attributes["data-state-char"].value;
